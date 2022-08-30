570ddd2140040c21672921457f3b222d78d46066
Piotr Gryko <pigryko@yahoo.com>
2022-08-30 22:44:58 +0100

    Added change of style on square click

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
 const Square = ({value}) => {
+
+    const handleClick = (e) => {
+        e.currentTarget.classList.add('wall')
+    }
+
   return (
-    <button className="square">
+    <button className="square" onClick={handleClick}>
         {value}
     </button>
   )
