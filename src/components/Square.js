9e0b1548148627f00a95e2e174807ff94986bbba
Piotr Gryko <pigryko@yahoo.com>
2022-08-30 22:57:49 +0100

    Added square value change on click

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,13 +2,16 @@ import React from 'react'
 
 const Square = ({value}) => {
 
+    let squareValue = 0
+
     const handleClick = (e) => {
         e.currentTarget.classList.add('wall')
+        e.currentTarget.innerHTML = 1
     }
 
   return (
     <button className="square" onClick={handleClick}>
-        {value}
+        {squareValue}
     </button>
   )
 }
