3ec13163ba7b1bbdd9f3eab618b8bcac4c671642
Piotr Gryko <pigryko@yahoo.com>
2022-09-01 22:32:04 +0100

    Working items on map

--- src/components/Square.js
+++ src/components/Square.js
@@ -8,18 +8,30 @@ const Square = ({currentItem}) => {
         console.log(currentItem)
 
         console.log("This is onClick return :" + currentItem[0])
-        e.currentTarget.classList.toggle(currentItem[1])
-        e.currentTarget.innerHTML = currentItem[0]
-        if (e.currentTarget.classList.contains(currentItem[1])) {
-            e.currentTarget.innerHTML = currentItem[0]
-        } else {
-            e.currentTarget.innerHTML = 0
-        }
+
+        if (e.currentTarget.innerText === "0") {
+            console.log("1st condition")
+            console.log(!e.currentTarget.classList.contains("empty"))
+            e.currentTarget.innerText = currentItem[0]
+            e.currentTarget.classList.remove("empty")
+            e.currentTarget.classList.add(currentItem[1])
         
+        } else if (e.currentTarget.innerText == (currentItem[0])){
+            e.currentTarget.innerText = "0"
+            e.currentTarget.className = ''
+            e.currentTarget.classList.add('square')    
+
+        } else if (e.currentTarget.innerText !== (currentItem[0]) && e.currentTarget.innerText !== "0") {
+            e.currentTarget.innerText = currentItem[0]
+            e.currentTarget.className = ''
+            e.currentTarget.classList.add('square')
+            e.currentTarget.classList.add(currentItem[1])
+        }
+
     }
 
   return (
-    <button className="square" onClick={handleClick}>
+    <button className="square empty" onClick={handleClick}>
         0
     </button>
   )
