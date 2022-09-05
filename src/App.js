f04438f5ee3ab52fa5e9857f0f92b19b6b56d869
Piotr Gryko <pigryko@yahoo.com>
2022-09-05 09:16:03 +0100

    Added camera helper

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import Form from './components/Form.js'
 import Grid from './components/Grid.js'
 import Legend from './components/Legend.js'
 import './App.css';
-import {Canvas, useThree} from "@react-three/fiber"
+import {Canvas, useThree, useFrame} from "@react-three/fiber"
 import Box from './components/Box.js';
-import { OrbitControls, PerspectiveCamera} from "@react-three/drei"
-import { Scene } from 'three';
+import { OrbitControls} from "@react-three/drei"
+import { Scene, DoubleSide, Mesh, PerspectiveCamera } from 'three';
 
 function App() {
 
@@ -53,22 +53,32 @@ const pull_data = (data) => {
   setMaze3d(data)
 }
 
+function CameraHelper() {
+  const camera = new PerspectiveCamera(60, 1, 1, 3);
+  return <group position={[0, 0, 2]}>
+    <cameraHelper args={[camera]} />
+  </group>;
+}
 
+function ThreeScene() {
+  return (
+      <Canvas className="canvas" camera={{position: [-12,12,0], fov: 60}}>
+        <OrbitControls enableZoom={true}/>
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[-2,5,2]} intensity={1} />
+        <Suspense name="maze-holder" id="suspense" fallback={null}>
+          {generate3dDungeon(maze3d)}
+        </Suspense>
+        <CameraHelper />
+      </Canvas>
+  );
+}
  
   return (
     <div className="App">
       <Form onAdd={addGrid}/>
       <Grid gridDim={gridDimensions} func={pull_data}/>
-      <Canvas className="canvas" camera={{position: [-12,12,0], fov: 60}}>
-        <OrbitControls enableZoom={true}/>
-        <ambientLight intensity={0.5} />
-        <directionalLight position={[-2,5,2]} intensity={1} />
-        <perspectiveCamera position={[0,0,0]}/>
-        <Suspense name="maze-holder" id="suspense" fallback={null}>
-          {generate3dDungeon(maze3d)}
-        </Suspense>
-
-      </Canvas>
+      <ThreeScene />
     </div>
   );
 }
